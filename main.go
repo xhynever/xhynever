042c8720b2aefcad508b379c132dde9733b99541
徐宏宇 <9859340+skysking@user.noreply.gitee.com>
2024-07-08 23:11:20 +0800

    7/8

--- main.go
+++ main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+const Pi float64 = 3.14159265358979323846
+const zero = 0.0
+
+// func add(args ...{}int) int {
+// 	sum := 0
+// 	for _, arg := range args {
+// 		sum += arg
+// 	}
+// 	return sum
+// }
+
+// func (s *Slice) Remove(value interface{}) error {
+// 	for i, v := range *s {
+// 		if isEqual(value, v) {
+// 			*s = append((*s)[:i], (*s)[i+1:]...)
+// 			return nil
+// 		}
+// 	}
+// 	return ERR_ELEM_NT_EXIST
+// }
+
+func main() {
+	// s := make([]int,0)
+	// a := []int64{1, 2, 3}
+	var flag bool
+	if flag == true {
+
+	}
+	// x := []int{1, 2, 3, 4, 5, 6,}
+	// b := add(a...)
+
+	// fmt.Println(b)
+	fmt.Printf("%T\n", zero)
+	fmt.Println(reflect.TypeOf(zero))
+	// fmt.Println(reflect.TypeOf(zero))
+}
---
+++ 常用技巧/面试题.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+// const类型，以及打印类型
+// const Pi float64 = 3.14159265358979323846
+// const zero = 0.0
+
+// func add(args ...{}int) int {
+// 	sum := 0
+// 	for _, arg := range args {
+// 		sum += arg
+// 	}
+// 	return sum
+// }
+
+// func main() {
+// a := []int64{1, 2, 3}
+
+// b := add(a...)
+
+// fmt.Println(b)
+// fmt.Printf("%T\n", zero)
+// fmt.Println(reflect.TypeOf(zero))
+// fmt.Println(reflect.TypeOf(zero))
+// }
+
+// bool类型赋值
+// var b bool
+
+// func main() {
+// 	//b = true
+// 	//b = 1
+// 	//b = bool(1)
+// 	//b = (1 == 2)
+// 	fmt.Println(b)
+// }
+
+
