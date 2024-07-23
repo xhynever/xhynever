2e4b5624e8ceee5ddc49f93d65aba8f9b2395bce
徐宏宇 <9859340+skysking@user.noreply.gitee.com>
2024-07-23 23:46:10 +0800

    7/23

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"reflect"
 )
 
 const Pi float64 = 3.14159265358979323846
@@ -37,20 +35,32 @@ func main() {
 	// x := []int{1, 2, 3, 4, 5, 6,}
 	// b := add(a...)
 
-	fmt.Println(b)
-	fmt.Printf("%T\n", zero)
-	fmt.Println(reflect.TypeOf(zero))
-	fmt.Println(reflect.TypeOf(zero))
+	// fmt.Println(b)
+	// fmt.Printf("%T\n", zero)
+	// fmt.Println(reflect.TypeOf(zero))
+	// fmt.Println(reflect.TypeOf(zero))
 
 	// 共享内存切片
 
-	a := []byte("AAA/BBB")
-	index := bytes.IndexByte(a, '/')
-	c := a[index+1:]
-	b := a[:index]
-	b = append(b, "CCC11"...)
-	fmt.Println(string(a), string(b), string(c))
+	// a := []byte("AAA/BBB")
+	// index := bytes.IndexByte(a, '/')
+	// c := a[index+1:]
+	// b := a[:index]
+	// b = append(b, "CCC11"...)
+	// fmt.Println(string(a), string(b), string(c))
 
-	fmt.Println(Pi)
+	// fmt.Println(Pi)
+	// 匿名函数执行后变量效果
+
+	var c int
+	c = 2
+	func(d int) {
+		var b = 1
+		fmt.Println(b)
+		c = d
+
+	}(3)
+	fmt.Println(c)
+	// fmt.Println(b)
 
 }
